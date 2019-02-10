1351ccba31227edf5c9b0625f97eccdd9667b2f0
Rcsaba <rcsprompt@gmail.com>
2019-02-11 00:41:04 +0100

    prepupulate users and questions table

--- app/User.php
+++ app/User.php
@@ -39,8 +39,5 @@ class User extends Authenticatable
     public function questions(){
         return $this->hasMany(Question::class);
     }
-    public function setTitleAttribute($value){
-        $this->attributes['title']=$value;
-        $this->attributes['slug']=str_slug($value);
-    }
+
 }
--- config/database.php
+++ config/database.php
@@ -42,6 +42,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'strict' => false,
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -52,7 +53,7 @@ return [
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
+
             'engine' => null,
         ],
 
