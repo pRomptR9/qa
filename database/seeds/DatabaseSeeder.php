b3cd50715d9a9dba371eee11e23875cc1e062776
Rcsaba <rcsprompt@gmail.com>
2019-02-11 17:36:47 +0100

    disp all q

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-       
+
         factory(App\User::class, 3)->create()->each(function($u){
             $u->questions()
                 ->saveMany(
