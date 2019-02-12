d1e5ebe0bbd81f0a36f28efefbefc76c2d7b3fe6
Rcsaba <rcsprompt@gmail.com>
2019-02-12 10:58:13 +0100

    Build Question Form

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,11 +5,33 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Dashboard</div>
+                    <div class="card-header">
+                        <div class="d-flex align-items-center">
+                            <h2>All Questions</h2>
+                            <div class="ml-auto">
+                                <a href="{{route('questions.create')}}"
+                                   class="btn btn-outline-secondary">Ask question</a>
+
+                            </div>
+                        </div>
+                    </div>
 
                     <div class="card-body">
                     @foreach($questions as $question)
                         <div class="media">
+                            <div class="d-flex flex-column counters">
+                                <div class="vote">
+                                    <strong>{{$question->votes}}</strong>
+                                    {{str_plural('vote', $question->votes)}}
+                                </div>
+                                <div class="status">
+                                    <strong>{{$question->votes}}</strong>
+                                    {{str_plural('answers', $question->answers)}}
+                                </div>
+                                <div class="view">
+                                    {{$question->views ." ". str_plural('view', $question->views)}}
+                                </div>
+                            </div>
                             <div class="media-body">
                                 <h3 class="mt-0">
                                     <a href="{{ $question->url }}">{{$question->title}}</a></h3>
